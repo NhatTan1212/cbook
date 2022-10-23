286367cbd2b0ae4231ed55758fdc22174aa0c87c
NhatTan1212 <116484948+NhatTan1212@users.noreply.github.com>
2022-10-23 15:27:42 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+<script>
+{/* var navMainMenu = document.querySelector(".nav-main-menu")
+window.onscroll = ()=>{
+    this.scrollY > 20 ? navMainMenu.classList.add("stricky"):
+    navMainMenu.classList.remove("stricky")
+} */}
+
+
+    {/* var slider-img = document.querySelector('.slider-img');
+    var imgs = ['banner2.png', 'banner3.png'];
+    var i = 0;
+
+    function  pre() {
+        if(i <= 0) i = images.length;
+        i--;
+        return setImg();
+    }
+
+    function next() {
+
+    }
+
+    function setImg() {
+        return slider-img.setAttribute('src', 'imgs/' + imgs[i]);
+    } */}
+</script>
+
